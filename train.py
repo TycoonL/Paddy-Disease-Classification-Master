af1343f81d38482e010a5aa265bb59b4261629a1
TycoonL <42563899+TycoonL@users.noreply.github.com>
2022-07-09 23:28:55 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -5,8 +5,9 @@ import torch.nn as nn
 import torch.optim as optim
 from model import CNN
 from torch.utils.data import DataLoader
-from torchvision import transforms, datasets
+from torchvision import transforms, datasets,models
 from torchvision.transforms import Lambda
+from check_accuracy import check_accuracy
 
 # 测试能否使用 GPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -18,26 +19,35 @@ num_classes = 10
 learning_rate = 0.001
 batch_size = 16
 num_epochs = 20
-h=640
-w=int(h*3/4)
+h=224
 
 model_path =None
 # 读取数据集
 
-transforms = transforms.Compose([
-    transforms.Resize(h),    # 将图片短边缩放至h，长宽比保持不变：
-    #这里如果不裁剪会出现RuntimeError: stack expects each tensor to be equal size, but got [3, 85, 64] at entry 0 and [3, 64, 85] 错误
-    #可能因为有些图片在裁剪前是翻转的
-    transforms.CenterCrop((h,w)),   #将图片从中心切剪
-
-    transforms.ToTensor(),          #把图片进行归一化，并把数据转换成Tensor类型
-    #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.RandomChoice([
+        transforms.Pad(padding=10),
+        transforms.CenterCrop(480),
+        transforms.RandomRotation(20),
+        transforms.CenterCrop((576, 432)),
+        transforms.ColorJitter(
+            brightness=0.1,
+            contrast=0.1,
+            saturation=0.1,
+            hue=0.1
+        )
+    ]),
+    transforms.Resize((h, h)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 #onehot 转换
 target_transform = Lambda(lambda y: torch.zeros(10, dtype=torch.float).scatter_(dim=0, index=torch.tensor(y), value=1))
 
 path = r'.\Paddy Doctor Dataset\train_images'
-data = datasets.ImageFolder(path,transforms)
+data = datasets.ImageFolder(path,transform)
 n = len(data)  # total number of examples
 n_test = random.sample(range(1, n), int(0.1 * n))  # take ~10% for test
 test_set = torch.utils.data.Subset(data, n_test)  # take 10%
@@ -49,16 +59,26 @@ data_test=DataLoader(test_set, batch_size=batch_size, shuffle=False)
 # for batch_idex, (data, targets) in enumerate(data_test):
 #     print(batch_idex,targets)
 
-# 实例化模型
-model = CNN(H=h,W=w,in_channels=in_channel,num_classes=num_classes)
+# # 实例化模型
+# model = CNN(H=h,in_channels=in_channel,num_classes=num_classes)
+# if model_path:
+#     model.load_state_dict(torch.load(model_path))
+# model.to(device)
+model = models.resnet34(pretrained=False)
+model.fc = nn.Sequential(
+    nn.Dropout(0.1),
+    nn.Linear(model.fc.in_features, 10)
+)
+model_path='resnet34_20.pth'
 if model_path:
-    model.load_state_dict(torch.load(model_path))
-model.to(device)
+     model.load_state_dict(torch.load(model_path))
+model = model.to(device)
 
 # 设定损失函数和优化器
 criterion = nn.CrossEntropyLoss()  #label不需要onehot，不需要softmax层
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
+model.train()
 # 下面部分是训练，有时可以单独拿出来写函数
 for epoch in range(num_epochs):
     for batch_idex, (data, targets) in enumerate(data_train):
@@ -75,32 +95,10 @@ for epoch in range(num_epochs):
         optimizer.step()
         if batch_idex % 20 == 0:
             print('Train Epoch: {} {} Loss: {:.6f}'.format(epoch,batch_idex,loss.item()))
-model_pth = 'D_640_model.pth'
+    check_accuracy(data_test, model)
+model_pth = 'resnet32_model.pth'
 torch.save(model.state_dict(), model_pth)
 
-# 评估准确度的函数
-def check_accuracy(loader, model):
-    # if loader.dataset.train:
-    #     print("Checking acc on training data")
-    # else:
-    #     print("Checking acc on testing data")
-    num_correct = 0
-    num_samples = 0
-    model.eval()  # 将模型调整为 eval 模式
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = y.to(device=device)
-            scores = model(x)
-            # 64*10
-            _, predictions = scores.max(1)
-            num_correct += (predictions == y).sum()
-            num_samples += predictions.size(0)
-        acc = float(num_correct) / float(num_samples) * 100
-        print(f'Got {num_correct} / {num_samples} with accuracy {acc:.2f}')
-
-    model.train()
-    return acc
 
 check_accuracy(data_train, model)
 check_accuracy(data_test, model)
